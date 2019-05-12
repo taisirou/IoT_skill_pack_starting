ba397226eb80fbf8f4bf8223bdc7572a019b850d
usashirou <32972270+taisirou@users.noreply.github.com>
2019-05-12 20:10:23 +0900

    Update IoT.py

--- IoT.py
+++ IoT.py
@@ -1,19 +1,54 @@
-# -*- Coding: utf-8 -*-
-
-
-import os
-import subprocess
+#! /usr/bin/env pyhton3
+ 
+from logging import getLogger
+logger = getLogger(__name__)
+ 
+import envirophat
+import ambient
+import time
 import datetime
-from time import sleep
-
-def main():
-    dt_now = datetime.datetime.now()
-    print(dt_now.strftime('%Y/%m/%d %H:%M:%S') + "Hello World from IoT device")
-    print(os.environ.get('LANG'))
-    #print(os.environ.get('RemoteENV'))
-
-
+import os
+ 
+AMBIENT_CHANNEL_ID = int(os.environ.get('AMBIENT_CHANNEL_ID'))
+AMBIENT_WRITE_KEY = os.environ.get('AMBIENT_WRITE_KEY')
+ 
+CHECK_SPAN = int(os.environ.get('CHECK_SPAN'), 30)
+ 
 if __name__ == '__main__':
+    from logging import StreamHandler
+    logger.addHandler(StreamHandler())
+ 
+    # create Ambient Object
+    am = ambient.Ambient(AMBIENT_CHANNEL_ID, AMBIENT_WRITE_KEY)
+ 
+    # main loop
     while True:
-        main()
-        sleep(5)
+        try:
+            # get acceleromener values
+            acc_values = [round(x,2) for x in envirophat.motion.accelerometer()]
+ 
+            # create send data
+            data = {
+                'created': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                'd1': round(envirophat.weather.temperature(), 1),
+                'd2': round(envirophat.weather.altitude(), 1),
+                'd3': round(envirophat.weather.pressure(unit='hPa'), 1),
+                'd4': envirophat.light.light(),
+                'd5': acc_values[0],
+                'd6': acc_values[1],
+                'd7': acc_values[2]
+            }
+ 
+            # send data
+            am.send(data)
+ 
+        except IOError:
+            # this is connection erro to enviro phat
+            time.sleep(CHECK_SPAN)
+            continue
+ 
+        except Exception as e:
+            logger.exception(e)
+ 
+        # wait time to next send timing
+        time.sleep(CHECK_SPAN)
